2fad639ffc030a6f5f862c3463d48bdb56790d14
Rodrigo Alves <rodrigoalves2014.ras@gmail.com>
2020-06-30 12:24:35 -0300

    Alterações no backend

---
+++ Backend/src/controllers/LeitosController.js
@@ -0,0 +1,39 @@
+const client = require('node-firebird');
+const options = require('../database/options');
+
+module.exports = {
+  async graphics(req,res){
+    const errorsData = "Não existe dados para ser retornados";
+    const sqlLeitos = "select * from PERCENTUAL_INDICATORS('')";
+    try{
+      await client.attach(options, (err,db)=>{
+        if(err){
+          throw err;
+        }else{
+            db.query(sqlLeitos, (err,result)=>{
+              try{
+                console.log('Buscando dados dos Leitos');
+                  this.leitos = result;
+
+                if(result != undefined){
+                  db.detach();
+                  for (let i = 0; i < leitos.length; i++) {
+                    const bf = (leitos[i]['LEITO1']).toString('utf8');
+                    leitos[i]['LEITO1'] = bf;
+                 }              
+                  return res.send({leitos});
+                }else
+                    res.status(400).send({errors:"Óbitos: "+errorsData});
+                }catch{
+                  throw err;
+                }
+              });     
+            }
+          });
+    }catch{
+        res
+        .status(400)
+        .send({error:"Falha ao carregar os dados dos usuarios do servidor"});
+      }
+  }
+}
